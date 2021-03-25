c4becb7f10f50b4ecf4786d5fa41f5d9ccfcbefd
ohfujimasa73 <ohfuji.masa73@gmail.com>
2021-03-25 21:28:15 +0900

    1213244244424

--- ConsoleApp5/Properties/AssemblyInfo.cs
+++ ConsoleApp5/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。2132312123
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
 [assembly: AssemblyTitle("ConsoleApp5")]
