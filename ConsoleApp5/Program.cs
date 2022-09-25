b29de4f2bb2437cd8efcc41f511a02f2b768e8f3
ohfujimasa73 <ohfuji.masa73@gmail.com>
2022-09-25 23:21:13 +0900

    ggggggggggggggggggggggggggggggg

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            // 入力を促すメッセージの表示して、文字を入力してもらうggghfghfhgfghgfhggggggggggggttttttttttjfgjtgkhktghkg
+            // 入力を促すメッセージの表示して、文字を入力してもらうggghfghfhgfghgfhggggggggggggttttttttttjfgjtgkhktghkgtyrtyrytryrtyttryry11111111111111111111
             Console.Write("あなたのお名前は？ : ");
             var name = Console.ReadLine();
 
